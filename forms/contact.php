baa03749483097595cd6879facfe25b5807aad47
Bcardsolutions <info@bigcardindia.com>
2021-03-27 10:51:09 +0530

    full website update with all the changes done

--- forms/contact.php
+++ forms/contact.php
@@ -7,12 +7,12 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'support@snotra.in';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'Snotra is unavailable right now, please try again later!');
   }
 
   $contact = new PHP_Email_Form;
